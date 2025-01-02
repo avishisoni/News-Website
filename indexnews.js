4f554546a88b20a5aac4fafc287558f38634d95a
avishisoni <avishisoni@gmail.com>
2025-01-02 14:38:52 +0530

    added titles

--- indexnews.js
+++ indexnews.js
@@ -1,7 +1,7 @@
 const apiKey = '1455b929237dacb1145484bb186cd80c';
 
 
-function createApiUrl(category,value) {
+function createApiUrl(category, value) {
     return `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=any&max=${value}&apikey=${apiKey}`;
 }
 //top 10 news
@@ -15,11 +15,11 @@ const SportsTNews = document.querySelector('.sportsRight')
 const HealthNews = document.querySelector('.Health')
 const BusinessNews = document.querySelector('.Business')
 const TechNews = document.querySelector('.technology')
-const Entertainment = document.querySelector('.entertainmentBody')
+const MoredetNews = document.querySelector('.moreNews')
+const Entertainment = document.querySelector('.entertainment')
 
-
-async function getWorldNews(category,value) {
- const url = createApiUrl(category,value)
+async function getWorldNews(category, value) {
+    const url = createApiUrl(category, value)
     const response = await fetch(url);
     if (!response.ok) {
         console.error('Failed to fetch data:', response.status, response.statusText);
@@ -29,9 +29,9 @@ async function getWorldNews(category,value) {
 
 }
 
-async function NewsData(category,value ) {
+async function NewsData(category, value) {
 
-    let topnews = await getWorldNews(category,value);
+    let topnews = await getWorldNews(category, value);
     {
         console.log('All articles:', topnews);
     }
@@ -52,7 +52,7 @@ async function NewsData(category,value ) {
         firstDes.textContent = firstarticle.description
     });
 
-    topnews.articles.slice(1).forEach((article) => {
+    topnews.articles.slice(1, 6).forEach((article) => {
         const toptennews = `
        <section class="top-news">
         <div class="top">
@@ -70,20 +70,32 @@ async function NewsData(category,value ) {
         </section>`
 
         topten.insertAdjacentHTML('beforeend', toptennews);
-    })}
-async function SportsData(category,value) {
-    let topsportsnews = await getWorldNews(category,value);
+    })
+    topnews.articles.slice(6, 12).forEach((article) => {
+        const morenews = `
+        <div class="moreNews">
+         <div class="moreDet">
+            <a>${article.title}</a>
+            <p>Continue Reading ></p>
+            <hr>
+        </div></div>`
+
+        MoredetNews.insertAdjacentHTML('beforeend', morenews);
+    })
+}
+async function SportsData(category, value) {
+    let topsportsnews = await getWorldNews(category, value);
     {
         console.log('All articles:', topsportsnews);
     }
     const firstsportsarticle = topsportsnews.articles[0];
-    document.querySelector('.sportsImg1').src=firstsportsarticle.image
-    document.querySelector('.sportsTitle').textContent=firstsportsarticle.title
-    document.querySelector('.SportsDes').textContent=firstsportsarticle.description
+    document.querySelector('.sportsImg1').src = firstsportsarticle.image
+    document.querySelector('.sportsTitle').textContent = firstsportsarticle.title
+    document.querySelector('.SportsDes').textContent = firstsportsarticle.description
 
 
 
-    topsportsnews.articles.slice(1,3).forEach((article) => {
+    topsportsnews.articles.slice(1, 3).forEach((article) => {
         const topSports = `<div class="sportsTab">
         <div class="sportsLeft">
             <img src="${article.image}" class="sportsImg">
@@ -91,8 +103,9 @@ async function SportsData(category,value) {
            </div>
         </div>`
 
-    SportsNews.insertAdjacentHTML('beforeend', topSports);})
-    topsportsnews.articles.slice(3,5).forEach((article) => {
+        SportsNews.insertAdjacentHTML('beforeend', topSports);
+    })
+    topsportsnews.articles.slice(3, 5).forEach((article) => {
         const topTSports = `<div class="sportsTab">
         <div class="sportsRight">
             <img src="${article.image}" class="sportsImg">
@@ -104,105 +117,113 @@ async function SportsData(category,value) {
     })
 
 
-}async function HealthData(category,value) {
-    let tophealthnews = await getWorldNews(category,value);
+} async function HealthData(category, value) {
+    let tophealthnews = await getWorldNews(category, value);
     {
         console.log('All articles:', tophealthnews);
     }
     tophealthnews.articles.slice(0).forEach((article) => {
         const tophealth = `
+<section class="cateNews"> 
 
        <div class="Health">
             <div class="news">
                 <img src="${article.image}">
                 <a>${article.title}</a>
                 
-            </div></div> <hr>`
+            </div></div> </section><hr>`
 
-            HealthNews.insertAdjacentHTML('beforeend', tophealth);
-    })}
-async function BusinessData(category,value) {
-    let topbusinessnews = await getWorldNews(category,value);
+        HealthNews.insertAdjacentHTML('beforeend', tophealth);
+    })
+}
+async function BusinessData(category, value) {
+    let topbusinessnews = await getWorldNews(category, value);
     {
         console.log('All articles:', topbusinessnews);
     }
     topbusinessnews.articles.slice(0).forEach((article) => {
         const topbusiness = `
-
-       <div class="Business">
+         <section class="cateNews"> 
+          <div class="Business">
             <div class="news">
                 <img src="${article.image}">
                 <a>${article.title}</a>
                 
-            </div><hr>`
+            </div></div></section><hr>`
 
-            BusinessNews.insertAdjacentHTML('beforeend', topbusiness);
-    })}
-    async function TechData(category,value) {
-        let toptechnews = await getWorldNews(category,value);
-        {
-            console.log('All articles:',  toptechnews);
-        }
-        toptechnews.articles.slice(0).forEach((article) => {
-            const toptech = `
-    
+        BusinessNews.insertAdjacentHTML('beforeend', topbusiness);
+    })
+}
+async function TechData(category, value) {
+    let toptechnews = await getWorldNews(category, value);
+    {
+        console.log('All articles:', toptechnews);
+    }
+    toptechnews.articles.slice(0).forEach((article) => {
+        const toptech = `
+    <section class="cateNews"> 
+
            <div class="technology">
                 <div class="news">
                     <img src="${article.image}">
                     <a>${article.title}</a>
                     
-                </div><hr>`
-    
-                TechNews.insertAdjacentHTML('beforeend', toptech);
-        })}
-        async function EntData(category,value) {
-            let topentnews = await getWorldNews(category,value);
-            {
-                console.log('All articles:',  topentnews);
-            }
-            topentnews.articles.slice(0).forEach((article) => {
-                const topent = `
-           <div class="entertainmentBody">
+                </div></div></section><hr>`
+
+        TechNews.insertAdjacentHTML('beforeend', toptech);
+    })
+}
+async function EntData(category, value) {
+    let topEntnews = await getWorldNews(category, value);
+    {
+        console.log('All articles:', topEntnews);
+    }
+    topEntnews.articles.slice(0).forEach((article) => {
+        const topEnt = `
+        <div class="entertainment">
+        <div class="entNews">
             <img src="${article.image}">
-            <a>${article.title}</a>
-            <span>${article.publishedAt}</span>
-        </div>`
-        
-                    Entertainment.insertAdjacentHTML('beforeend', topent);
-            })}
-            
-            
-Entertainment.innerHTML=""
-TechNews.innerHTML=""
-BusinessNews.innerHTML=""
-HealthNews.innerHTML=""
-topten.innerHTML = "" 
-SportsNews.innerHTML = "" 
-SportsTNews.innerHTML = "" 
+            <a href="${article.url}">${article.title}</a>
+            <p>Continue Reading ></p>
+        </div></div>
+    
+           `
+
+        Entertainment.insertAdjacentHTML('beforeend', topEnt);
+    })
+}
+
+TechNews.innerHTML = ""
+BusinessNews.innerHTML = ""
+HealthNews.innerHTML = ""
+topten.innerHTML = ""
+SportsNews.innerHTML = ""
+SportsTNews.innerHTML = ""
 
 
 async function loadGeneralNews() {
-    await NewsData('general','6');
+    await NewsData('general', '12');
 }
-async function loadEntNews() {
-    await EntData('entertainment','5');
-}
+
 async function loadHealthNews() {
-    await HealthData('health','4');
+    await HealthData('health', '4');
 }
 async function loadSportsNews() {
-    await SportsData('sports','5');
+    await SportsData('sports', '5');
 }
 async function loadBusinessNews() {
-    await BusinessData('business','4');
+    await BusinessData('business', '4');
 }
 async function loadTechNews() {
-    await TechData('technology','4');
+    await TechData('technology', '4');
 }
-loadEntNews();
+async function loadEntNews() {
+    await EntData('entertainment', '5');
+}
 loadTechNews();
 loadBusinessNews();
 loadHealthNews();
 loadGeneralNews();
 loadSportsNews();
+loadEntNews();
 
